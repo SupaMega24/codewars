9389cf3883b0dd53450b8eb91fe241ef05246ff6
SupaMega24 <138974329+SupaMega24@users.noreply.github.com>
2023-12-11 19:26:30 +0900

    Add challenges and  solutions

--- 8kyu/addItemToArray.js
+++ 8kyu/addItemToArray.js
@@ -3,9 +3,8 @@
 
 // The websites array has already been defined for you using the following code:
 
-// var websites = [];
 
-
+var websites = [];
 
 
 // add the value "codewars" to the already defined websites array
