346ec3d2a2506bd8ec1bc2040ee10bf86a9137e6
SupaMega24 <138974329+SupaMega24@users.noreply.github.com>
2024-03-27 22:30:00 +0900

    Add solution

---
+++ JavaScript/8kyu/numberObjects.js
@@ -0,0 +1,29 @@
+// Coding in function whatNumberIsIt. function accept 1 parameter:n. it's a number.
+
+// To judge the number n. If n is one of the above five constants, return one of these string:
+
+// "Input number is Number.MAX_VALUE"
+// "Input number is Number.MIN_VALUE"
+// "Input number is Number.NaN"
+// "Input number is Number.NEGATIVE_INFINITY"
+// "Input number is Number.POSITIVE_INFINITY"
+// Other values should return "Input number is xxx". xxx means this number.
+
+
+// MY SOLUTION
+const whatNumberIsIt = n => {
+    switch (true) {
+        case n === Number.MAX_VALUE:
+            return "Input number is Number.MAX_VALUE";
+        case n === Number.MIN_VALUE:
+            return "Input number is Number.MIN_VALUE";
+        case isNaN(n):
+            return "Input number is Number.NaN";
+        case n === Number.NEGATIVE_INFINITY:
+            return "Input number is Number.NEGATIVE_INFINITY";
+        case n === Number.POSITIVE_INFINITY:
+            return "Input number is Number.POSITIVE_INFINITY";
+        default:
+            return "Input number is " + n;
+    }
+}
