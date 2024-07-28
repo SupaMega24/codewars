9d0c4e64943810894192aaab02a44759d6b880bc
SupaMega24 <138974329+SupaMega24@users.noreply.github.com>
2024-07-28 20:54:29 +0900

    Add solution

---
+++ JavaScript/8kyu/whatIsBetween.js
@@ -0,0 +1,14 @@
+
+https://www.codewars.com/kata/55ecd718f46fba02e5000029/solutions/javascript
+
+//*****************************//
+//******** MY SOLUTION ********//
+//*****************************//
+
+function between(a, b) {
+    let result = [];
+    for (let i = a; i <= b; i++) {
+        result.push(i);
+    }
+    return result;
+}
